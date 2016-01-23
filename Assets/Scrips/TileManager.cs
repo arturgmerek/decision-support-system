1892557d9b5bcfaf1cec2c259640921678167e08
Artur Gmerek <arturgmerek@gmail.com>
2016-01-23 14:37:48 +0100

    sync with laptop

---
+++ Assets/Scrips/TileManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+     * TODO:
+     * Create controller converting mouse input into grid values and so on...
+     */
+
+}
